29277088cc3738e9b277f82bf5be08b7f796d522
privedenie1748@mail.ru <privedenie1748@mail.ru>
2020-01-24 14:50:38 +0300

    Сделал 4 задания , не смог 5,6

--- task02/task02.py
+++ task02/task02.py
@@ -1,1 +1,4 @@
+def fun(a, b, c, d, e, g):
+    return print(a + " " + b + " " + c + " " + d + " " + e + " " + g)
+print(fun(a = input("введите имя "), b = input("введите фамилию "), c = input("введите город "), d = input("введите год рождения "), e = input("введите email "), g = input("введите номер телеыона ")))
 
