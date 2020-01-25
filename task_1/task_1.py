c78da6f97d3135cfa653a96a559fda2708d0244d
0leg-zharkov <privedenie1748@mail.ru>
2020-01-25 13:06:27 +0300

    .

--- task_1/task_1.py
+++ task_1/task_1.py
@@ -3,7 +3,7 @@ f = lambda  : n_1 / n_2
 try:
  n_1 = int(input())
  n_2 = int(input())
-except division by zero:
+except division by zero: print('division by zero')
 print((lambda n_1, n_2: n_1 / n_2)(n_1, n_2))
 
 
